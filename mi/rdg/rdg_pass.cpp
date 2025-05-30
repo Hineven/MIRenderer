e7f0c96a28559f90be64d67947fc94add4b8d4e9
hineven@pku.edu.cn <hinevenwob@qq.com>
2025-05-30 16:08:40 +0800

    fix: add nullptr checks for pass resource usage

--- mi/rdg/rdg_pass.cpp
+++ mi/rdg/rdg_pass.cpp
@@ -26,7 +26,7 @@ RDGPass::~RDGPass() {
 }
 
 RDGPass * RDGPass::AddTexture(RDGTexture *texture, RDGTextureUsageType usage) {
-    // assert(!is_compiled_);
+    if (!texture) return this; // Do nothing if the texture is null
     if (usage != RDGTextureUsageType::kTransferDst) {
         compiled_.in_textures.emplace_back(texture);
     }
@@ -46,7 +46,7 @@ RDGPass * RDGPass::AddTexture(RDGTexture *texture, RDGTextureUsageType usage) {
 }
 
 RDGPass * RDGPass::AddBuffer(RDGBuffer *buffer, RHIGPUAccessFlags access) {
-    // assert(!is_compiled_);
+    if (!buffer) return this; // Do nothing if the buffer is null
     if (access & RHIGPUAccessFlagBits::kRead) compiled_.in_buffers.emplace_back(buffer);
     if (access & RHIGPUAccessFlagBits::kWrite) compiled_.out_buffers.emplace_back(buffer);
     compiled_.used_buffers.emplace_back(access, buffer);
