ba2e64dfccaccfc705628f2e36c9bf04edc06ef5
hineven@pku.edu.cn <hinevenwob@qq.com>
2026-01-18 10:25:34 +0800

    fix: TAA在tonemap压缩空间后进行混合与clamping

--- mi/renderer/shaders/TAA.hlsl
+++ mi/renderer/shaders/TAA.hlsl
@@ -19,6 +19,16 @@ Texture2D<float4> G_Normal; // packed in 0..1
 [[vk::image_format("rgba16f")]]
 RWTexture2D<float4> RWRadianceTexture;
 
+float3 Map(float3 color) {
+    // Reinhard tone mapping
+    return color / (dot(color, float3(0.2126f, 0.7152f, 0.0722f)) + 1.0f);
+}
+
+float3 ReverseMap(float3 color) {
+    // Reverse Reinhard tone mapping
+    return color / (1.0f - dot(color, float3(0.2126f, 0.7152f, 0.0722f)));
+}
+
 float4 ClampHistory(float4 hist, float2 uv) {
     // Gather 3x3 neighborhoo statistics
     float3 m1 = 0, m2 = 0;
@@ -29,14 +39,15 @@ float4 ClampHistory(float4 hist, float2 uv) {
         [unroll]
         for(int i=-1;i<=1;i++) {
             float2 uvn = uv + float2(i,j) / UB.FilmDimensions;
-            float4 c = CurrentRadianceTexture.SampleLevel(PointEdgeSampler, uvn, 0);
+            float4 Sample = CurrentRadianceTexture.SampleLevel(PointEdgeSampler, uvn, 0);
+            float3 c = Map(Sample.rgb);
             float3 yc = RGBToYCoCg(c.rgb);
             m1 += yc;
             m2 += yc * yc;
             
             // For Alpha, strict Min/Max is often more stable than Variance for binary masks
-            min_a = min(min_a, c.w);
-            max_a = max(max_a, c.w);
+            min_a = min(min_a, Sample.w);
+            max_a = max(max_a, Sample.w);
         }
     }
     float3 mu = m1 / 9.0f;
@@ -81,7 +92,7 @@ void TAA_Main(uint2 tid: SV_DispatchThreadID)
     if(any(tid >= C.FilmDimensions)) return;
     float2 uv = (tid + 0.5f) * C.InvFilmDimensions;
     float4 CurrentSample = CurrentRadianceTexture.Load(uint3(tid,0));
-    float3 CurrRGB = CurrentSample.rgb;
+    float3 CurrRadiance  = CurrentSample.rgb;
     
     // Use velocity dilation to find the most prominent motion in the neighborhood
     float2 mv = GetClosestDepthMotionVector(int2(tid));
@@ -96,7 +107,10 @@ void TAA_Main(uint2 tid: SV_DispatchThreadID)
     }
 
     float4 PrevSample = PreviousRadianceTexture.SampleLevel(PointEdgeSampler, PrevUV, 0);
-    float3 PrevRGB = PrevSample.rgb;
+    float3 PrevRadiance = PrevSample.rgb;
+    // Compress to tone-mapped space for better clamping and ghosting removal for bright areas
+    float3 PrevRGB = Map(PrevRadiance);
+    float3 CurrRGB = Map(CurrRadiance);
 
     // Simple neighborhood clamp in YCoCg
     float3 CurrYCoCg = RGBToYCoCg(CurrRGB);
@@ -110,6 +124,7 @@ void TAA_Main(uint2 tid: SV_DispatchThreadID)
     float4 OutVec = lerp(TargetVec, ClampedHist, UB.BlendFactor);
 
     float3 OutRGB = YCoCgToRGB(OutVec.rgb);
-    RWRadianceTexture[tid] = float4(OutRGB, OutVec.w);
+    float3 OutRadiance = ReverseMap(OutRGB);
+    RWRadianceTexture[tid] = float4(OutRadiance, OutVec.w);
 }
 
